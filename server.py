30935b5b2339adb4397aefabbe4967fe28ed8739
dreessen-n <dreessen.edu@gmail.com>
2022-06-27 14:36:20 -0600

    Updated server.py and working on logic in index.thml

--- server.py
+++ server.py
@@ -1,10 +1,30 @@
+import random
 from flask import Flask, render_template, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "The Grand Wahzoo will now make a guess."
+
+count = 0
 
 @app.route('/')
 def index():
+    # Get computer's random guess
+    if 'comp_guess' and 'count' in session:
+        session['count'] += 1
+    else:
+        session['comp_guess'] = random.randint(1, 100)
     return render_template('index.html')
+
+@app.route('/user_guess', methods=['POST'])
+def save_user_guess():
+    session['user_guess'] = int(request.form['user_guess'])
+    print(f"user guess is: {session['user_guess']}")
+    return redirect('/')
+
+@app.route('/end_session')
+def end_session():
+    session.clear()
+    return redirect('/')
 
 # Error message for 404
 @app.errorhandler(404)
